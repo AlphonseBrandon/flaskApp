5919a906159925ff58fa9031ea2d7d7b83aacf67
AlphonseBrandon <alphonse.brand@gmail.com>
2021-09-13 14:23:41 +0100

    Model has been set up

--- awwblog/blog/models.py
+++ awwblog/blog/models.py
@@ -19,3 +19,9 @@ class Post(models.Model):
         updated = models.DateTimeField(auto_now_add=True)
 
         status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+        class Meta:
+            ordering = ('-publish',)
+
+        def __str__(self):
+            return self.title

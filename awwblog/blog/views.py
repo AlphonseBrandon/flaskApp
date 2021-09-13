1cc7ee591ab81c2d8d8a7c590c6b292237a74ecf
AlphonseBrandon <alphonse.brand@gmail.com>
2021-09-13 15:39:10 +0100

    Created single post view

--- awwblog/blog/views.py
+++ awwblog/blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Post
 
 def post_list(request):
-    posts = Post.objects.filter(status="published")
+    posts = Post.published.all()
     return render(request, 'post_list.html',{'posts':posts})
+
+def post_detail(request, post):
+    post = get_object_or_404(Post, slug=post, status='published')
+    return render(request, 'post_detail.html', {'post':post})

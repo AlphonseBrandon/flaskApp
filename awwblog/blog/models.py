15d734c31500f45f3c408ad3c8f5439a90505669
AlphonseBrandon <alphonse.brand@gmail.com>
2021-09-13 14:09:37 +0100

    creating a meta class

--- awwblog/blog/models.py
+++ awwblog/blog/models.py
@@ -8,3 +8,14 @@ class Post(models.Model):
         ('draft', 'Draft'),
         ('published', 'Published'),
         )
+
+        title = models.CharField(max_length=250)
+        slug = models.SlugField(max_length=250, unique_for_date='publish')
+        author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_post')
+        body = models.TextField()
+
+        publish = models.DateTimeField(default=timezone.now)
+        created = models.DateTimeField(auto_now_add=True)
+        updated = models.DateTimeField(auto_now_add=True)
+
+        status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
